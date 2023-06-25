7d2dc7ae34c53480921c8a3a80255d3d56d38614
Aitor Gallardo <38296940+AitorGallardo@users.noreply.github.com>
2023-06-25 23:43:59 +0200

    base app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountryListComponent } from './components/country-list/country-list.component';
+import { CountryCardComponent } from './components/country-card/country-card.component';
+import { CountryPageComponent } from './components/country-page/country-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountryListComponent,
+    CountryCardComponent,
+    CountryPageComponent
   ],
   imports: [
     BrowserModule,
